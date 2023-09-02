26ce835c9ef4f9b34771bacf23ac301dc965e9c1
lucas <lucas1ferreira@hotmail.com>
2023-09-02 15:49:46 -0300

    introduce to append and write files

--- files/crud_file.py
+++ files/crud_file.py
@@ -5,3 +5,26 @@ print(os.path.abspath(file1.name))
 print(os.path.realpath(file2.name))
 print(file1.read())
 file1.close(); file2.close()
+
+
+with open("file2.txt",'w') as file2:
+    message = "linha1\rlinha2\rlinha3"
+    file2.write(message)
+
+
+def readLines(file):
+    with open(file,"r") as file_read:
+        for linha in file_read:
+            print(linha)
+
+readLines('file2.txt')
+
+print(" \r\r------ proximo passo APPEND --------\r\r ")
+
+def appendText(file,message):
+    with open(file,"a") as file_write_append:
+        file_write_append.write(f"\r{message}")
+
+appendText('file2.txt','Funcinou!!!')
+
+readLines('file2.txt')
